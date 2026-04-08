65dc3969808b71c7b4c9be43b010741b8d93a306
龔靖之(Jin Kung) <2311016@iisigroup.com>
2026-04-08 16:19:02 +0800

    調整臉書分享方式

--- app/pages/result.vue
+++ app/pages/result.vue
@@ -308,6 +308,9 @@ const shareToFB = async () => {
         if (navigator.canShare && navigator.canShare({ files: [file] })) {
           await navigator.share({
             files: [file],
+            title: "咖哩靈魂拌測驗！測出你的咖哩人格",
+            text: "測出你的咖哩人格，解鎖你命定的咖哩配方，就有機會獲得 Apple Watch 等大禮！",
+            url: shareUrl,
           });
         } else {
           window.location.href = url;
