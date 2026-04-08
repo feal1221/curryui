f44842369e46b85b2443b71c84a55f5a31199b06
龔靖之(Jin Kung) <2311016@iisigroup.com>
2026-04-08 16:59:09 +0800

    看看有沒有預覽文字

--- app/app.vue
+++ app/app.vue
@@ -28,8 +28,8 @@ useSeoMeta({
   ogTitle: '咖哩靈魂拌測驗',
   description: '面對咖哩的世紀爭議，你的「拌哩」哲學是什麼？快來測測你的咖哩人格！',
   ogDescription: '透過 6 題性格直覺測驗，找出最能代表你生活態度的咖哩風味。',
-  // ogImage: 'https://example.com/quiz-cover.jpg', // 替換成你的測驗封面圖
-  twitterCard: 'summary_large_image',
+  ogImage: 'https://test.housefindyourcurry.tw/images/share.png', // 替換成你的測驗封面圖
+  ogType: 'website'
 })
 useHead({
   meta: [
